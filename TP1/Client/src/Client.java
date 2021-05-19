3726c92c8434ed2715ba1d131c6ccc2c1e89f838
David Murray <david.murray16@hotmail.com>
2021-05-19 19:16:25 -0400

    Add code from class notes

--- TP1/Client/src/Client.java
+++ TP1/Client/src/Client.java
@@ -1,10 +1,26 @@
+import java.io.DataInputStream;
+import java.net.Socket;
+
 
 public class Client {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static Socket socket;
+	
+	public static void main(String[] args) throws Exception {
 		System.out.println("Client start!");
-
+		
+		String serverAddr = "127.0.0.1";
+		int port = 5002;
+		
+		socket = new Socket(serverAddr, port);
+		System.out.format("The server is running on %s:%d", serverAddr, port);
+		
+		DataInputStream in = new DataInputStream(socket.getInputStream());
+		
+		String helloMessage = in.readUTF();
+		System.out.println(helloMessage);
+		
+		socket.close();
 	}
 
 }
--- TP1/Server/src/Server.java
+++ TP1/Server/src/Server.java
@@ -1,10 +1,71 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
+	
+	private static ServerSocket listener;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
 		System.out.println("Server start!");
 
+		int clientNumber = 0;
+		
+		String serverAddress = "127.0.0.1";
+		int serverPort = 5002;
+		
+		listener = new ServerSocket();
+		listener.setReuseAddress(true);
+		InetAddress serverIP = InetAddress.getByName(serverAddress);
+		
+		listener.bind(new InetSocketAddress(serverIP, serverPort));
+		
+		System.out.format("The server is running on %s:%d", serverAddress, serverPort);
+		
+		try {
+			while (true) {
+				new ClientHandler(listener.accept(), clientNumber++).start();
+			}
+		} finally {
+			listener.close();
+		}
+	}
+	
+	
+	private static class ClientHandler extends Thread
+	{
+		private Socket socket;
+		private int clientNumber;
+
+		public ClientHandler(Socket socket, int clientNumber) {
+			this.socket = socket;
+			this.clientNumber = clientNumber;
+
+			System.out.println("New connection with client #" + clientNumber + " at " + socket);
+		}
+
+		public void run() {
+			try
+			{
+				DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+				out.writeUTF("Hello from server - you are client #" + clientNumber);
+
+			} catch (IOException e) {
+				System.out.println("Error handling client #" + clientNumber + ": " + e);
+			} finally {
+				try {
+					socket.close();
+				} catch (IOException e) {
+					System.out.println("Could not close socket.");
+				}
+
+				System.out.println("Connection with client # " + clientNumber + " closed");
+			}
+		}
 	}
 
 }
