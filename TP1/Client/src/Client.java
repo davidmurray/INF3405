d996017ed9e2e1a4d519ba303bb98f05ec88abf1
David Murray <david.murray16@hotmail.com>
2021-05-26 17:19:40 -0400

    Re-format code

--- TP1/Client/src/Client.java
+++ TP1/Client/src/Client.java
@@ -4,59 +4,60 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.regex.Pattern;
 
-
 public class Client {
 
 	private static Socket socket;
-	
+
 	public static void main(String[] args) throws Exception {
 		System.out.println("Client start!");
-		
+
 		String serverAddr = null;
 
 		while (true) {
-    	    System.out.print("Enter the IP address of the server: ");
+			System.out.print("Enter the IP address of the server: ");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			serverAddr = reader.readLine();
-			
+
 			if (!isValidIPv4Address(serverAddr)) {
-	    	    System.out.println("The IP address you have entered is not a valid IPv4 address. Please try again. ");
-	    	    continue;
+				System.out.println("The IP address you have entered is not a valid IPv4 address. Please try again. ");
+				continue;
 			} else {
 				break;
 			}
 		}
-		
+
 		int port = 0;
 		while (true) {
-    	    System.out.print("Enter the port to connect to: ");
+			System.out.print("Enter the port to connect to: ");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			port = Integer.parseInt(reader.readLine());
-			
+
 			if (!isValidPort(port)) {
-	    	    System.out.println("The port number you have entered is not a valid (must be between 5000 and 5050). Please try again. ");
-	    	    continue;
+				System.out.println(
+						"The port number you have entered is not a valid (must be between 5000 and 5050). Please try again. ");
+				continue;
 			} else {
 				break;
 			}
 		}
-		
+
 		socket = new Socket(serverAddr, port);
 		System.out.format("The server is running on %s:%d", serverAddr, port);
-		
+
 		DataInputStream in = new DataInputStream(socket.getInputStream());
-		
+
 		String helloMessage = in.readUTF();
 		System.out.println(helloMessage);
-		
+
 		socket.close();
 	}
-	
+
 	private static boolean isValidIPv4Address(String IP) {
-    	Pattern ipPattern = Pattern.compile("^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
-    	return ipPattern.matcher(IP).matches();
+		Pattern ipPattern = Pattern
+				.compile("^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
+		return ipPattern.matcher(IP).matches();
 	}
-	
+
 	private static boolean isValidPort(int port) {
 		return (port >= 5000 && port <= 5050);
 	}
--- TP1/Server/src/Server.java
+++ TP1/Server/src/Server.java
@@ -9,50 +9,51 @@ import java.net.Socket;
 import java.util.regex.Pattern;
 
 public class Server {
-	
+
 	private static ServerSocket listener;
 
 	public static void main(String[] args) throws Exception {
 		System.out.println("Server start!");
 
 		int clientNumber = 0;
-		
+
 		String serverAddress = null;
 		while (true) {
-    	    System.out.print("Enter the IP address to listen on: ");
+			System.out.print("Enter the IP address to listen on: ");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			serverAddress = reader.readLine();
-			
+
 			if (!isValidIPv4Address(serverAddress)) {
-	    	    System.out.println("The IP address you have entered is not a valid IPv4 address. Please try again. ");
-	    	    continue;
+				System.out.println("The IP address you have entered is not a valid IPv4 address. Please try again. ");
+				continue;
 			} else {
 				break;
 			}
 		}
-		
+
 		int serverPort = 0;
 		while (true) {
-    	    System.out.print("Enter the port number to listen on: ");
+			System.out.print("Enter the port number to listen on: ");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			serverPort = Integer.parseInt(reader.readLine());
-			
+
 			if (!isValidPort(serverPort)) {
-	    	    System.out.println("The port number you have entered is not a valid (must be between 5000 and 5050). Please try again. ");
-	    	    continue;
+				System.out.println(
+						"The port number you have entered is not a valid (must be between 5000 and 5050). Please try again. ");
+				continue;
 			} else {
 				break;
 			}
 		}
-		
+
 		listener = new ServerSocket();
 		listener.setReuseAddress(true);
 		InetAddress serverIP = InetAddress.getByName(serverAddress);
-		
+
 		listener.bind(new InetSocketAddress(serverIP, serverPort));
-		
+
 		System.out.format("The server is running on %s:%d", serverAddress, serverPort);
-		
+
 		try {
 			while (true) {
 				new ClientHandler(listener.accept(), clientNumber++).start();
@@ -61,18 +62,18 @@ public class Server {
 			listener.close();
 		}
 	}
-	
+
 	private static boolean isValidIPv4Address(String IP) {
-    	Pattern ipPattern = Pattern.compile("^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
-    	return ipPattern.matcher(IP).matches();
+		Pattern ipPattern = Pattern
+				.compile("^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
+		return ipPattern.matcher(IP).matches();
 	}
-	
+
 	private static boolean isValidPort(int port) {
 		return (port >= 5000 && port <= 5050);
 	}
-	
-	private static class ClientHandler extends Thread
-	{
+
+	private static class ClientHandler extends Thread {
 		private Socket socket;
 		private int clientNumber;
 
@@ -84,8 +85,7 @@ public class Server {
 		}
 
 		public void run() {
-			try
-			{
+			try {
 				DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
 				out.writeUTF("Hello from server - you are client #" + clientNumber);
